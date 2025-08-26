808607998f2f19c00306d327fe702e85c729ffd4
Matias Nicolas Arregui <matiasarregui2@gmail.com>
2025-08-26 12:10:48 -0300

    mejoras

--- app1/urls.py
+++ app1/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inicio/', views.listaProyectos.as_view(), name='proyectos'),
-    path('inicio/proyecto/<int:pk>/', views.proyectoDetalle, name='proyectoDetalle'),
+    path('', views.listaProyectos.as_view(), name='proyectos'),
+    path('proyecto/<int:pk>/', views.proyectoDetalle, name='proyectoDetalle'),
 ]
